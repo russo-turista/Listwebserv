d3de6d1ddf51ab92ca0e767e1fedb31bd3934d19
Gena <fjobmail@gmail.com>
2014-04-14 21:37:45 +0400

    Local commit

--- logic/src/main/java/com/listwebserv/service/UserServiceImpl.java
+++ logic/src/main/java/com/listwebserv/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.listwebserv.service;
 
 import java.sql.Timestamp;
 import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.dao.SaltSource;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
@@ -29,17 +30,10 @@ public class UserServiceImpl implements UserService  {
     }
 
 	@Override
-<<<<<<< HEAD:logic/src/main/java/com/listwebserv/security/UserServiceImpl.java
-public  void setUser(User user) {		
-		listservDAO.addUser(user.getName(), user.getLogin(), passwordEncoder.encodePassword(user.getPassword(), null),
-					user.getEmail(), new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()),
-				    user.getActive(),user.getAdmin());
-=======
 	public  void setUser(User user) {		
 		listservDAO.addUser(user.getName(), user.getLogin(), passwordEncoder.encodePassword(user.getPassword(), null), user.getEmail(),
 					        new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()),
 				            user.getActive(),user.getAdmin());
->>>>>>> eaa8205c4935c198c43e66344bdf98fd3d1720d9:logic/src/main/java/com/listwebserv/service/UserServiceImpl.java
 	}
 	
 }
